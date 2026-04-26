cd4d9cd9b0ffdf9618f363bd76a079556e29aa5e
Muhammad Bilal <muhammadbilalmscs@gmail.com>
2026-04-26 11:44:29 +0200

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreApplicationStocksData.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
